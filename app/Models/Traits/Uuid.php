9ecb7d093c3fc9fb3b20f618d6e45ceaa85fef88
Michel Tank <micheltank@outlook.com>
2022-02-06 20:14:56 +0100

    feat: created category resource

---
+++ app/Models/Traits/Uuid.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Traits;
+use \Ramsey\Uuid\Uuid as RamseyUuuid;
+
+trait Uuid {
+    public static function boot() {
+        parent::boot();
+        static::creating(function($obj) {
+            $obj->id = RamseyUuuid::uuid4()->toString();
+        });
+    }
+}
