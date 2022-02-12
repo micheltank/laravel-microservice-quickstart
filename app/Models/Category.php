77902efa724d2e1a8fe5f8fefa24e80c8a28ac42
Michel Tank <micheltank@outlook.com>
2022-02-12 20:58:02 +0100

    unit/integration tests

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,9 +8,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use SoftDeletes, Traits\Uuid;
+
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = [
-        'id' => 'string'
+        'id' => 'string',
+        'is_active' => 'boolean'
     ];
+    public $incrementing = false;
+
 }
